8f22ef28e3077418757b9eac99c5e413cc5be458
Federico Iachetti <iachetti.federico@gmail.com>
2021-11-09 15:34:43 -0300

    Use person parameter type everywhere

--- features/support/person_parameter.rb
+++ features/support/person_parameter.rb
@@ -2,6 +2,8 @@ ParameterType(
   name:        "person",
   regexp:      /Sean|Lucy|Larry/,
   transformer: -> (name) {
+    @people ||= {}
+
     @people[name] ||= Shouty::Person.new(name, @network, 0)
   }
 )
